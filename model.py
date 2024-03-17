e7011af6b3c0fde381cba3a0040c950693f47ead
md-equram-ansari <equram786ansari12@gmail.com>
2024-03-17 14:40:14 +0530

    lstm added

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from sklearn.discriminant_analysis import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import tweepy
 import math
 import nltk
@@ -11,10 +12,12 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
-
 from datetime import datetime
 from datetime import timedelta
 from textblob import TextBlob
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from sklearn.preprocessing import MinMaxScaler
 import time
 
 nltk.download('punkt')
@@ -34,91 +37,6 @@ def moving_avg(df):
     df['rets'] = df['Close'] / df['Close'].shift(1) - 1
 
     return df
-
-
-def make_predictions(df):
-    df['HL_PCT'] = (df['High'] - df['Low']) / df['Close'] * 100.0
-    df['PCT_change'] = (df['Close'] - df['Open']) / df['Open'] * 100.0
-
-    # Drop missing values
-    df.fillna(value=-99999, inplace=True)
-
-    # We want to separate 1 percent of the data to forecast
-    forecast_out = int(math.ceil(0.01 * len(df)))
-
-    # Separating the label here, we want to predict the AdjClose
-    forecast_col = 'Adj Close'
-    df['label'] = df[forecast_col].shift(-forecast_out)
-    X = df.drop(['label'], axis=1)  # Corrected drop function call
-    X = StandardScaler().fit_transform(X)  # Scale the features
-    X_forecast = X[-forecast_out:]
-    X = X[:-forecast_out]
-
-    # Separate label and identify it as y
-    y = df['label'].values[:-forecast_out]
-
-    # Split data
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=0)
-
-    # Linear regression
-    model = LinearRegression(n_jobs=-1)
-    model.fit(X_train, y_train)
-
-    # KNN Regression
-    model_knn = KNeighborsRegressor(n_neighbors=2)
-    model_knn.fit(X_train, y_train)
-
-    # Bayesian Ridge Regression
-    model_by = BayesianRidge()
-    model_by.fit(X_train, y_train)
-
-    # Create confidence scores
-    confidencereg = model.score(X_test, y_test)
-    confidence_model_knn = model_knn.score(X_test, y_test)
-    confidence_model_by = model_by.score(X_test, y_test)
-
-    reg = confidencereg * 100
-    knn = confidence_model_knn * 100
-    by = confidence_model_by * 100
-
-    score = "Regression {}\nKNN {}\nBayesian {}\n".format(reg, knn, by)
-
-    # Create new columns for forecasts
-    df['Forecast_reg'] = np.nan
-    df['Forecast_knn'] = np.nan
-    df['Forecast_by'] = np.nan
-
-    # Generate forecasts
-    forecast_reg = model.predict(X_forecast)
-    forecast_knn = model_knn.predict(X_forecast)
-    forecast_by = model_by.predict(X_forecast)
-
-    # Fill in forecast columns with predicted values
-    # last_date = df.index[-1]
-    # next_date = last_date + timedelta(days=1)
-    # Assuming the date format is 'YYYY-MM-DD'
-    last_date = datetime.strptime(df.index[-1], '%Y-%m-%d')
-    next_date = last_date + timedelta(days=1)
-
-    for i in forecast_reg:
-        df.loc[next_date, 'Forecast_reg'] = i
-        next_date += timedelta(days=1)
-
-    last_date = df.index[-26]
-    next_date = last_date + timedelta(days=1)
-    for i in forecast_knn:
-        df.loc[next_date, 'Forecast_knn'] = i
-        next_date += timedelta(days=1)
-
-    last_date = df.index[-26]
-    next_date = last_date + timedelta(days=1)
-    for i in forecast_by:
-        df.loc[next_date, 'Forecast_by'] = i
-        next_date += timedelta(days=1)
-
-    return df
-
 
 def make_predictions(df):
 
@@ -159,21 +77,40 @@ def make_predictions(df):
     model_by = BayesianRidge()
     model_by.fit(X_train, y_train)
 
+
+    # LSTM regression
+    model_lstm = Sequential()
+    model_lstm.add(LSTM(units=50, activation='relu', input_shape=(X_train.shape[1], 1)))
+    model_lstm.add(Dense(units=1))
+    model_lstm.compile(optimizer='adam', loss='mean_squared_error')
+
+    # Reshape X_train and X_test for LSTM
+    X_train_lstm = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+    X_test_lstm = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+    model_lstm.fit(X_train_lstm, y_train, epochs=10, batch_size=32)
+
     # Create confindence scores
     confidencereg = model.score(X_test, y_test)
     confidence_model_knn = model_knn.score(X_test, y_test)
     confidence_model_by = model_by.score(X_test, y_test)
+    # LSTM requires a different approach to obtain confidence
+    confidence_model_lstm = model_lstm.evaluate(X_test_lstm, y_test)
 
     reg = confidencereg * 100
     knn = confidence_model_knn * 100
     by = confidence_model_by * 100
+    lstm = (1 - confidence_model_lstm) * 100  # LSTM returns loss, so use (1 - loss) as confidence
 
-    score = " Regression {}\n KNN {}\n Bayesian {}\n ".format(reg, knn, by)
-
+    score = "Regression {}\nKNN {}\nBayesian {}\nLSTM {}\n".format(reg, knn, by, lstm)
     # Create new columns
     forecast_reg = model.predict(X_forecast)
     forecast_knn = model_knn.predict(X_forecast)
     forecast_by = model_by.predict(X_forecast)
+
+    # Reshape X_forecast for LSTM
+    X_forecast_lstm = np.reshape(X_forecast, (X_forecast.shape[0], X_forecast.shape[1], 1))
+    forecast_lstm = model_lstm.predict(X_forecast_lstm)
 
     # Process all new columns data
     df['Forecast_reg'] = np.nan
@@ -209,6 +146,19 @@ def make_predictions(df):
         next_date = next_unix
         next_unix += timedelta(days=1)
         df['forecast_by'].loc[next_date] = i
+
+    df['forecast_lstm'] = np.nan
+
+    last_date = df.iloc[-26].name
+    last_unix = last_date
+    next_unix = last_unix + timedelta(days=1)
+
+    for i in forecast_lstm:
+        next_date = next_unix
+        next_unix += timedelta(days=1)
+        df['forecast_lstm'].loc[next_date] = i
+
+
 
     return df
 
